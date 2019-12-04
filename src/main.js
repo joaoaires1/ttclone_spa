0397dd97007adf912b494ac40b4af209064b3f68
João Aires <airesjoao25@outlook.com>
2019-12-03 22:34:47 -0300

    add axios with baseurl

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$http = axios.create({
+  baseURL: 'http://127.0.0.1:8000/api'
+});
+
 new Vue({
-  store,
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
