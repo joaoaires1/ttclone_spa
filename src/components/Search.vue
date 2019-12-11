22c95ea32d78f97ec85a7fb9aa888aa9a5d3623c
João Aires <airesjoao25@outlook.com>
2019-12-10 23:04:42 -0300

    search component feature

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,58 @@
 <template>
-    <div class="search">
-        <span>
-            <img src="../assets/lupa.svg" width="20px" alt="">
-        </span>
-        <input type="text" placeholder="Search on Twitter">
+    <div>
+        <div class="search">
+            <span>
+                <img src="../assets/lupa.svg" width="20px" alt="">
+            </span>
+            <input v-model="name" @keyup="getUsers" type="text" placeholder="Search on Twitter">
+        </div>
+
+        <div v-if="results.length > 0" class="results">
+            <div class="result-row" v-for="item in results" :key="item.id">
+                <div class="result-avatar">
+                    <img src="../assets/avatar.png" alt="">
+                </div>
+                <div class="result-name">
+                    <div class="name">{{ item.name }}</div>
+                    <div class="username">@{{ item.username }}</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+
+    data() {
+        return {
+            user: '',
+            name: '',
+            results: []
+        }
+    },
+    methods: {
+        getUsers () {
+
+            if ( this.name.length > 0 ) {
+                this.$http.get('/search', { params: {
+                    id: this.user.id,
+                    api_token: this.user.api_token,
+                    name: this.name
+                } })
+                .then( res => {
+                    this.results = res.data
+                } )
+            } else {
+                this.results = []
+            }
+            
+        }
+    },
+    created() {
+        const isAuthenticated = JSON.parse(localStorage.getItem('authenticatedUser'))
+        this.user = isAuthenticated
+    }
 
 }
 </script>
@@ -38,6 +82,43 @@ export default {
     border-left: none;
     border-top-right-radius: 50px;
     border-bottom-right-radius: 50px;
+}
+
+.results {
+    border: 1px solid #eee;
+    border-radius: 5px;
+}
+
+.result-row {
+    display: flex;
+    padding: 10px;
+    border-bottom: 1px solid #eee;
+}
+
+.result-avatar img {
+    width: 40px;
+    border-radius: 50px;
+}
+
+.result-name {
+    display: flex;
+    flex-flow: wrap;
+    /* align-items: stretch; */
+    padding-left: 10px;
+}
+
+.result-name div {
+    flex: 1 100%;
+}
+
+.name {
+    font-weight: bold;
+    letter-spacing: 0.05em;
+}
+
+.username {
+    font-size: 14px;
+    color: rgb(101, 119, 134);
 }
 
 </style>
